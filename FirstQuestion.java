2a63966b42c0f4c588a7ae693d98b97d301e6280
duggemvs <111142028+duggemvs@users.noreply.github.com>
2022-11-18 15:28:43 +0530

    Update FirstQuestion.java

--- FirstQuestion.java
+++ FirstQuestion.java
@@ -3,6 +3,13 @@ public class FirstQuestion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int arr[]= new int[] {1,2,3,1,2,3,4,5,4};
+		for(int i = 0; i < arr.length; i++) {  
+            for(int j = i + 1; j < arr.length; j++) {  
+                if(arr[i] == arr[j])  
+                    System.out.println(arr[j]);  
+            }  
+        }  
 
 	}
 
